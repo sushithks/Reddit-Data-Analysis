c7355a551fb0e4faa4260ccdf69c56c323aaabdc
sushith <sushithks@gmail.com>
2025-05-30 18:58:08 -0400

    Post extract function

--- etl/reddit.py
+++ etl/reddit.py
@@ -15,3 +15,12 @@ def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
     except Exception as e:
         print(e)
         sys.exit(1)
+
+
+def extract_posts(reddit_instance: Reddit, subreddit: str, time_filter: str, limit=None):
+    subreddit = reddit_instance.subreddit(subreddit)
+    posts = subreddit.top(time_filter=time_filter, limit=limit)
+
+    post_lists = []
+
+    return post_lists
