8b01e5167685db409c0a990eb4b25d6d05dc4608
sushith <sushithks@gmail.com>
2025-06-01 17:13:23 -0400

    Transformation

--- etl/reddit.py
+++ etl/reddit.py
@@ -30,3 +30,8 @@ def extract_posts(reddit_instance: Reddit, subreddit: str, time_filter: str, lim
         post_lists.append(post)
 
     return post_lists
+
+def transform_data(post_df: pd.DataFrame):
+    post_df['created_utc'] = pd.to_datetime(post_df['created_utc'], unit='s')
+    post_df['over_18'] = np.where((post_df['over_18'] == True), True, False)
+    post_df['author'] = post_df['author'].astype(str)
