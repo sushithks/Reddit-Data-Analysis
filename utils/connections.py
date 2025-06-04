ccbd33fd7516447103053d809dc655f40b61be4e
sushith <sushithks@gmail.com>
2025-06-04 19:11:25 -0400

    utils file

--- utils/connections.py
+++ utils/connections.py
@@ -3,3 +3,7 @@ import os
 
 parser = configparser.ConfigParser()
 parser.read(os.path.join(os.path.dirname(__file__), '../config/config.conf'))
+
+
+SECRET = parser.get('api_keys', 'secret_key')
+CLIENT_ID = parser.get('api_keys', 'client_id')
