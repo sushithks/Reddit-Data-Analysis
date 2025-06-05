bb7b1002221e3c47e318338c2359992dafe35c6a
sushith <sushithks@gmail.com>
2025-06-05 17:31:21 -0400

    utils file - DB Key

--- utils/connections.py
+++ utils/connections.py
@@ -5,8 +5,8 @@ parser = configparser.ConfigParser()
 parser.read(os.path.join(os.path.dirname(__file__), '../config/config.conf'))
 
 
-SECRET = parser.get('api_keys', 'secret_key')
-CLIENT_ID = parser.get('api_keys', 'client_id')
+SECRET = parser.get('api_keys', 'reddit_secret_key')
+CLIENT_ID = parser.get('api_keys', 'reddit_client_id')
 
 DATABASE_HOST =  parser.get('database', 'database_host')
 DATABASE_NAME =  parser.get('database', 'database_name')
