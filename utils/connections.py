fe249692d61b912148f909ac72670cb451100cde
sushith <sushithks@gmail.com>
2025-06-05 17:30:01 -0400

    utils file - AWS

--- utils/connections.py
+++ utils/connections.py
@@ -13,3 +13,11 @@ DATABASE_NAME =  parser.get('database', 'database_name')
 DATABASE_PORT =  parser.get('database', 'database_port')
 DATABASE_USER =  parser.get('database', 'database_username')
 DATABASE_PASSWORD =  parser.get('database', 'database_password')
+
+#AWS
+AWS_ACCESS_KEY_ID = parser.get('aws', 'aws_access_key_id')
+AWS_ACCESS_KEY = parser.get('aws', 'aws_secret_access_key')
+AWS_BUCKET_NAME = parser.get('aws', 'aws_bucket_name')
+
+INPUT_PATH = parser.get('file_paths', 'input_path')
+OUTPUT_PATH = parser.get('file_paths', 'output_path')
