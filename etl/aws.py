4a71160c6b5a99459a6803adb6eb2c0a56cefe43
sushith <sushithks@gmail.com>
2025-06-08 18:52:19 -0400

    AWS connect - S3 Try block

--- etl/aws.py
+++ etl/aws.py
@@ -2,8 +2,10 @@ import s3fs
 from utils.connections import AWS_ACCESS_KEY_ID, AWS_ACCESS_KEY
 
 def connect_to_s3():
-    s3 = s3fs.S3FileSystem(anon=False,
+    try:
+        s3 = s3fs.S3FileSystem(anon=False,
                                key= AWS_ACCESS_KEY_ID,
                                secret=AWS_ACCESS_KEY)
-    return s3
-    print(e)
+        return s3
+    except Exception as e:
+        print(e)
