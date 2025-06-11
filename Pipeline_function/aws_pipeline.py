2d322817235f907dd78a2e37bdf4dcf0c7f8dd47
sushith <sushithks@gmail.com>
2025-06-11 18:33:58 -0400

    pipeline functions - Create bucket

--- Pipeline_function/aws_pipeline.py
+++ Pipeline_function/aws_pipeline.py
@@ -1,4 +1,4 @@
-from etl.aws import connect_to_s3, upload_to_s3
+from etl.aws import connect_to_s3, create_bucket_if_not_exist,  upload_to_s3
 from utils.connections import AWS_BUCKET_NAME
 
 
@@ -6,4 +6,5 @@ def upload_s3_pipeline(ti):
     file_path = ti.xcom_pull(task_ids='reddit_extraction', key='return_value')
 
     s3 = connect_to_s3()
+    create_bucket_if_not_exist(s3, AWS_BUCKET_NAME)
     upload_to_s3(s3, file_path, AWS_BUCKET_NAME, file_path.split(',')[-1])
