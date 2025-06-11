7dc8380dc8585ca8105b0fb26eebdc67508db340
sushith <sushithks@gmail.com>
2025-06-11 18:34:44 -0400

    pipeline functions - '/' Delimiter

--- Pipeline_function/aws_pipeline.py
+++ Pipeline_function/aws_pipeline.py
@@ -7,4 +7,4 @@ def upload_s3_pipeline(ti):
 
     s3 = connect_to_s3()
     create_bucket_if_not_exist(s3, AWS_BUCKET_NAME)
-    upload_to_s3(s3, file_path, AWS_BUCKET_NAME, file_path.split(',')[-1])
+    upload_to_s3(s3, file_path, AWS_BUCKET_NAME, file_path.split('/')[-1])
