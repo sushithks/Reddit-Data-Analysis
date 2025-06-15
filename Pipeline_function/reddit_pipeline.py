fa792775cfdd9e0c4dd31b3ef89e1d884ccf361d
sushith <sushithks@gmail.com>
2025-06-14 20:39:29 -0400

    reddit Dag -1 pipeline

--- Pipeline_function/reddit_pipeline.py
+++ Pipeline_function/reddit_pipeline.py
@@ -12,7 +12,7 @@ def reddit_pipeline(file_name: str, subreddit: str, time_filter='day', limit=Non
     post_df = pd.DataFrame(posts)
     # transformation
     post_df = transform_data(post_df)
-    # loading to csv
+
     file_path = f'{OUTPUT_PATH}/{file_name}.csv'
     load_data_to_csv(post_df, file_path)
 
